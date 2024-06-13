c4e1af6f6e1a82f34b41916090354c4c208529f7
whybe-choi <choi980912@khu.ac.kr>
2024-06-14 00:23:14 +0900

    feat: Define Encoder

--- model.py
+++ model.py
@@ -112,3 +112,39 @@ class MultiHeadAttentionBlock(nn.Module):
 
         # (batch, seq_len, d_model) -> (batch, seq_len, d_model)
         return self.w_o(x)
+
+class ResidualConnection(nn.Module):
+
+    def __init__(self, dropout: float):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x))) # In the paper, apply LayerNormalizaion after sub-layer
+
+
+class EncoderBlock(nn.Module):
+    
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+        x = self.residual_connections[1](x, lambda x: self.feed_forward_block(x))
+        return x
+    
+class Encoder(nn.Modulue):
+
+    def __init__(self, layers: nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
