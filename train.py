a404909be13accf37b1ea0bb5a971add3a974f3b
whybe-choi <choi980912@khu.ac.kr>
2024-06-27 17:52:46 +0900

    feat: Define train_model func

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from torch.utils.data import Dataset, DataLoader, random_split
 
 from dataset import BiligualDataset, causal_mask
 from model import build_transformer
+from config import get_config, get_weights_file_path
 
 from datasets import load_dataset
 from tokenizers import Tokenizer
@@ -11,6 +12,10 @@ from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
+import wandb
+
+import warnings
+from tqdm import tqdm
 from pathlib import Path
 
 def get_all_sentences(ds, lang):
@@ -64,3 +69,93 @@ def get_ds(config):
 def get_model(config, vocab_src_len, vocab_tgt_len):
     model = build_transformer(vocab_src_len, vocab_tgt_len, config['seq_len'], config['seq_len'], config['d_model'])
     return model
+
+def train_model(config):
+    # Define the device
+    device = torch.device("cuda" if torch.cude.is_available() else "cpu")
+    print(f"Using device {device}")
+
+    Path(config['model_folder']).mkdir(parents=True, exist_ok=True)
+
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"], eps=1e-9)
+
+    initial_epoch = 0
+    global_step = 0
+    if config["preload"]:
+        model_filename = get_weights_file_path(config, config["preload"])
+        print(f"Preloading model {model_filename}")
+        state = torch.load(model_filename)
+        model.load_state_dict(state["model_state_dict"])
+        initial_epoch = state["epoch"] + 1
+        optimizer.load_state_dict(state["optimizer_state_dict"])
+        global_step = state["global_step"]
+        del state
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id("[PAD]"), label_smoothing=0.1).to(device)
+
+    # define our custom x axis metric
+    wandb.define_metric("global_step")
+    # define which metrics will be plotted against it
+    wandb.define_metric("validation/*", step_metric="global_step")
+    wandb.define_metric("train/*", step_metric="global_step")
+
+    for epoch in range(initial_epoch, config['num_epochs']):
+        model.train()
+        batch_iterator = tqdm(train_dataloader, desc=f"Processing epoch {epoch:02d}")
+        for batch in batch_iterator:
+            encoder_input = batch["encoder_input"].to(device) # (batch, seq_len)
+            decoder_input = batch["decoder_input"].to(device) # (batch, seq_len)
+            encoder_mask = batch["encoder_mask"].to(device) # (batch, 1, 1, seq_len)
+            decoder_mask = batch["decoder_mask"].to(device) # (batch, 1, seq_len, seq_len)
+
+            # Run the tensors through the transformer
+            encoder_output = model.encoder(encoder_input, encoder_mask) # (batch, seq_len, d_model)
+            decoder_output = model.decoder(encoder_output, encoder_mask, decoder_input, decoder_mask) # (batch, seq_len, d_model)
+            proj_output = model.project(decoder_output) # (batch, seq_len, tgt_vocab_size)
+
+            label = batch["label"].to(device) # (batch, seq_len)
+
+            # (batch, seq_len, tgt_vocab_size) -> (batch * seq_len, tgt_vocab_size)
+            loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1,))
+            batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
+
+            # Log the loss
+            wandb.log({"train/loss": loss.item(), "global_step": global_step})
+
+            # BackPropagate the loss
+            loss.backward()
+
+            # Update the weights
+            optimizer.step()
+            optimizer.zero_grad(set_to_none=True)
+
+            global_step += 1
+
+        # Save the model at the end of every epoch
+        model_filename = get_weights_file_path(config, f"{epoch:02d}")
+        torch.save({
+            "epoch": epoch,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "global_step": global_step,
+        }, model_filename)
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+
+    config = get_config()
+    config["epoch"] = 30
+    config["preload"] = None
+
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="pytorch-transformer", 
+        
+        # track hyperparameters and run metadata
+        config=config
+    )
+
+    train_model(config)
